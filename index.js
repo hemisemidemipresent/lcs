f89a417cdea07022ed6e9c04499a30d3e36e5fb7
hemisemidemipresent <hnngggrrrr@gmail.com>
2022-04-24 11:30:39 +0800

    broom

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
-const body = document.getElementById('body');
-console.log(body);
-let img = document.createElement('img');
-img.src = './LCI.png';
-body.appendChild(img);
+let data;
+let frame = 0;
+let imgs = makeImgs();
+function makeImgs() {
+    let imgs = [];
+    for (let x = 0; x < 30; x++) {
+        for (let y = 0; y < 23; y++) {
+            let img = document.createElement('img');
+            img.id = `${x},${y}`;
+            img.src = './LCS.png';
+
+            body.appendChild(img);
+            imgs.push(img);
+        }
+    }
+    return imgs;
+}
+axios
+    .get('https://raw.githubusercontent.com/hemisemidemipresent/lcs/main/lcs.txt')
+    .then(function (response) {
+        data = response.data;
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+    .then(function () {
+        function nextFrame() {
+            let framedata = data[frame];
+            for (let i = 0; i < framedata.length; i++) {
+                let image = imgs[i];
+                if (framedata[i]) image.src = './LCI.png';
+                else image.src = './LCS.png';
+            }
+            console.log(frame);
+            frame++;
+            window.requestAnimationFrame(nextFrame);
+        }
+        window.requestAnimationFrame(nextFrame);
+    });
