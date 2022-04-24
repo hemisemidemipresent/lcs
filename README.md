006898c9da4ada1ed5be1b9c36bbf0e42024a01c
hemisemidemipresent <hnngggrrrr@gmail.com>
2022-04-24 10:57:47 +0800

    what am I doing

---
+++ README.md
@@ -0,0 +1,9 @@
+# yes
+
+`LCX` = `LCS` or `LCI`
+
+480px wide, 368px tall.
+
+30 `LCX` wide, 23 `LCX` tall
+
+each `LCX` occupies 16px
---
+++ index.js
@@ -0,0 +1,5 @@
+const body = document.getElementById('body');
+console.log(body);
+let img = document.createElement('img');
+img.src = './LCI.png';
+body.appendChild(img);
