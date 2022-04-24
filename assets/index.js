e38ac5c498b62a7ed5c6f072c782679db67515cb
hemisemidemipresent <hnngggrrrr@gmail.com>
2022-04-24 16:32:36 +0800

    aa

---
+++ assets/index.js
@@ -0,0 +1,61 @@
+let data;
+let imgs = makeImgs();
+function makeImgs() {
+    let imgs = [];
+    for (let x = 0; x < 30; x++) {
+        for (let y = 0; y < 23; y++) {
+            let img = document.createElement('img');
+            img.src = 'assets/LCS.png';
+            body.appendChild(img);
+            imgs.push(img);
+        }
+    }
+    return imgs;
+}
+function download() {
+    axios
+        .get('https://raw.githubusercontent.com/hemisemidemipresent/lcs/main/data/lcs-new.txt')
+        .then(function (response) {
+            data = response.data;
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .then(function () {
+            document.getElementById('download').style.visibility = 'hidden';
+            document.getElementById('play').style.visibility = 'visible';
+            document.getElementById('body').style.visibility = 'visible';
+        });
+}
+
+const FRAMES_PER_SECOND = 30; // Valid values are 60,30,20,15,10...
+// set the mim time to render the next frame
+const FRAME_MIN_TIME = (1000 / 60) * (60 / FRAMES_PER_SECOND) - (1000 / 60) * 0.5;
+let lastFrameTime = 0; // the last frame time
+let frame = 0;
+
+function nextFrame(time) {
+    if (time - lastFrameTime < FRAME_MIN_TIME) {
+        //skip the frame if the call is too early
+        requestAnimationFrame(nextFrame);
+        return; // return as there is nothing to do
+    }
+    lastFrameTime = time; // remember the time of the rendered frame
+
+    // render the frame
+
+    let framedata = data[frame];
+    for (let i = 0; i < framedata.length; i++) {
+        let image = imgs[i];
+        if (framedata[i] > 200) image.src = 'assets/LCI.png';
+        else image.src = 'assets/LCS.png';
+    }
+    frame++;
+
+    requestAnimationFrame(nextFrame); // get next farme
+}
+function play() {
+    document.getElementById('play').style.visibility = 'hidden';
+
+    window.requestAnimationFrame(nextFrame);
+}
---
+++ hd.js
@@ -0,0 +1,62 @@
+let data;
+let imgs = makeImgs();
+function makeImgs() {
+    let imgs = [];
+    for (let x = 0; x < 60; x++) {
+        for (let y = 0; y < 46; y++) {
+            let img = document.createElement('img');
+            img.src = 'assets/LCS.png';
+            body.appendChild(img);
+            imgs.push(img);
+        }
+    }
+    return imgs;
+}
+function download() {
+    axios
+        .get('https://raw.githubusercontent.com/hemisemidemipresent/lcs/main/lcs-hd.txt')
+        .then(function (response) {
+            data = response.data;
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .then(function () {
+            document.getElementById('download').style.visibility = 'hidden';
+            document.getElementById('play').style.visibility = 'visible';
+            document.getElementById('body').style.visibility = 'visible';
+        });
+}
+
+const FRAMES_PER_SECOND = 5; // Valid values are 60,30,20,15,10...
+// 6x slower than normal
+// set the mim time to render the next frame
+const FRAME_MIN_TIME = (1000 / 60) * (60 / FRAMES_PER_SECOND) - (1000 / 60) * 0.5;
+let lastFrameTime = 0; // the last frame time
+let frame = 0;
+
+function nextFrame(time) {
+    if (time - lastFrameTime < FRAME_MIN_TIME) {
+        //skip the frame if the call is too early
+        requestAnimationFrame(nextFrame);
+        return; // return as there is nothing to do
+    }
+    lastFrameTime = time; // remember the time of the rendered frame
+
+    // render the frame
+
+    let framedata = data[frame];
+    for (let i = 0; i < framedata.length; i++) {
+        let image = imgs[i];
+        if (framedata[i] > 200) image.src = 'assets/LCI.png';
+        else image.src = 'assets/LCS.png';
+    }
+    frame++;
+
+    requestAnimationFrame(nextFrame); // get next farme
+}
+function play() {
+    document.getElementById('play').style.visibility = 'hidden';
+
+    window.requestAnimationFrame(nextFrame);
+}
